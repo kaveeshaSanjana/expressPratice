3bdd2d4d8eaca28761b18a7eb6c2698ed746e2c0
kaveeshaSanjana <kapilakarunarathna@gmail.com>
2025-05-17 15:04:46 +0530

    implement server.mjs and install nodemon for dev live sync

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello, World!');} 
+);     
+
+server.listen(4000, () =>{
+    console.log('Server is running on port 4000');
+});
